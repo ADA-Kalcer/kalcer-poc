4082719891a50e78b359f66c83ecf14a81b97066
Tude Maha <gede_mahardika88@yahoo.co.id>
2025-08-12 14:22:23 +0800

    feat: setup MVVM

--- KalcerPOC/KalcerPOCApp.swift
+++ KalcerPOC/KalcerPOCApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct KalcerPOCApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
---
+++ KalcerPOC/ViewModel/ViewModelTemplate.swift
@@ -0,0 +1,8 @@
+//
+//  Template.swift
+//  KalcerPOC
+//
+//  Created by Tude Maha on 12/08/2025.
+//
+
+import Foundation
