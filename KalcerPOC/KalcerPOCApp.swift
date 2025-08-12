a5048bc189a999eca611efcbf58fac2efad6db5e
Tude Maha <gede_mahardika88@yahoo.co.id>
2025-08-12 14:08:57 +0800

    feat: add project base

---
+++ KalcerPOC/KalcerPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  KalcerPOCApp.swift
+//  KalcerPOC
+//
+//  Created by Tude Maha on 12/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct KalcerPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
